b4958a1ef59fe66fd046372bb9d0b9ac6dce1cda
Kip Dunlap <kdunlap@atlantaga.gov>
2022-06-16 14:23:23 -0400

    clean up unused files

--- Tables in the air/app.js
+++ Tables in the air/app.js
@@ -2,13 +2,14 @@ var Airtable = require('airtable');
 var base = new Airtable({ apiKey: 'keyDFa7RNG5otUO3C' }).base('appMDLXS91cMvJWid');
 
 base('NPU Master List').select({
+  fields: ["NPU", "Status"],
   maxRecords: 25,
   view: "NPU Information Sheet"
 }).eachPage(function page(records, fetchNextPage) {
   // This function (`page`) will get called for each page of records.
 
   records.forEach(function (record) {
-    console.log('Retrieved', record.get('NPU'));
+    console.log(record.get('NPU'), record.get('Status'));
   });
 
       // If there are no more records, `done` will get called.
