a838ca7110ecef4b060bdaa642489f8de582e7e1
Kip <88916382+UrbaneD0ge@users.noreply.github.com>
2022-08-13 00:38:52 -0400

    posts, but can't grab string values

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var Airtable = require('airtable');
 var base = new Airtable({ apiKey: 'keyDFa7RNG5otUO3C' }).base('appotIP5Ss3YUKYYR');
 
-let NPU = document.getElementById('NPU').selectedValue;
+let NPU = document.getElementById('NPU').value;
+let name = document.querySelector('#name').value;
+let itmType = document.querySelector('#itmType').selectedValue;
 
 // on submit, get the values from the form
 document.getElementById('submit').addEventListener('click', function (event) {
@@ -9,8 +11,8 @@ document.getElementById('submit').addEventListener('click', function (event) {
     {
       fields: {
         "fldSdIFMSRkdJGd9Z": 'NPU-' + NPU + '_' + new Date().toLocaleDateString(),
-        "fldck9li8kMT9xBLx": "LRB",
-        "fldYxaSmdxSjS1N0k": "O'Drinkyhan's",
+        "fldck9li8kMT9xBLx": itmType.value(),
+        "fldYxaSmdxSjS1N0k": name,
         "fldBuDdWpnXqlmr9T": ["Approval"],
         "fldswanafOKIWEGy3": "Applicant was asked to construct additional parking so that patrons will be incentivized to drink and drive.",
         "fldMb04KSFvOpBXf9": [""]
@@ -20,6 +22,7 @@ document.getElementById('submit').addEventListener('click', function (event) {
     if (err) { console.error(err); return; }
     records.forEach(function (record) {
       console.log(record.getId());
+      console.log(record.fields);
     }
     );
   }
