635cd4152461ba2ce1751112b8cdf4d29651beb6
Kip <Kipling.Dunlap@gmail.com>
2022-08-13 14:57:21 -0400

    oh yeah. timing matters.

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 var Airtable = require('airtable');
 var base = new Airtable({ apiKey: 'keyDFa7RNG5otUO3C' }).base('appotIP5Ss3YUKYYR');
 
-let NPU = document.getElementById('NPU').value;
-let name = document.querySelector('#name').value;
-let itmType = document.querySelector('#itmType').selectedValue;
 
 // on submit, get the values from the form
 document.getElementById('submit').addEventListener('click', function (event) {
+  let NPU = document.getElementById('NPU').value;
+  let name = document.getElementById('name').value;
+  let itmType = document.getElementById('itmType').value;
   base('Table 1').create([
     {
       fields: {
         "fldSdIFMSRkdJGd9Z": 'NPU-' + NPU + '_' + new Date().toLocaleDateString(),
-        "fldck9li8kMT9xBLx": itmType.value(),
+        "fldck9li8kMT9xBLx": `${itmType}`,
         "fldYxaSmdxSjS1N0k": name,
         "fldBuDdWpnXqlmr9T": ["Approval"],
         "fldswanafOKIWEGy3": "Applicant was asked to construct additional parking so that patrons will be incentivized to drink and drive.",
